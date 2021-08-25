435fec4192966888b75b13ac2e1320c51c5a83c0
septst <parthibansekar87@gmail.com>
2021-08-25 23:46:11 +0530

    Update server.spec.js

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -6,11 +6,13 @@ describe('calc', () => {
     })
 })
 
+//not working
 describe('get messages should return 200 OK', () => {
     it('should return 200', () => {
         request.get('http://localhost:3000/messages', (err, res) => {
             console.log(res.body);
             expect(res.statusCode).toEqual(200);
+            done();
         })
     })
 })
@@ -18,9 +20,8 @@ describe('get messages should return 200 OK', () => {
 describe('get messages should return messages', () => {
     it('should return more than one message', () => {
         request.get('http://localhost:3000/messages', (err, res) => {
-            console.log('Begin');
-            console.log(res.body.length);
             expect(res.body.length).toBeGreaterThan(1);
+            done();
         })
     })
 })
